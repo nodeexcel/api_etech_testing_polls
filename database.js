c490b9dd2e20a3461050d62fb0723217343e518b
ashutosh_m <ashutosh_m@excellencetechnologies.in>
2017-11-09 14:25:35 +0530

    mlab db added

--- database.js
+++ database.js
@@ -1,21 +1,21 @@
 var mongoose = require('mongoose');
 
-module.exports = function(mongoose) {
+module.exports = function (mongoose) {
 
-    var conn = mongoose.createConnection('mongodb://ashutosh_m:java123@ds251245.mlab.com:51245/etech_testing_polls');
-
-
+    var conn = mongoose.createConnection('mongodb: //localhost/etech_testing_api');
+    
+    
     var users = mongoose.Schema({}, {
         strict: false,
         collection: 'users'
     });
     var table_users = conn.model('users', users);
+    
+    
+    console.log( table_users );
+    
 
-
-    console.log(table_users);
-
-
-    return function(req, res, next) {
+    return function (req, res, next) {
         req.table_users = table_users;
         next();
     }
